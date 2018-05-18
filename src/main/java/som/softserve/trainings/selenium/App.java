84082fa6acef76f16b3b6a60bb0f24743f7ccbcf
Yermek Kadyrbayev <kek277@gmail.com>
2018-05-18 08:10:44 +0300

    first commit

---
+++ src/main/java/som/softserve/trainings/selenium/App.java
@@ -0,0 +1,13 @@
+package som.softserve.trainings.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
